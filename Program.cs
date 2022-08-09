f9acacb1f00f7525209498c0715767ea6f206137
Darya <daryalas17@gmail.com>
2022-08-09 12:48:11 +0300

    seminar53

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int[] array = getArray(10, 0, 20);
+
+int[] getArray(int size, int min, int max)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+    Console.WriteLine("Массив: {0}{1}{2}", "[", String.Join(", ", result), "]");
+    return result;
+}
+
+int Diff(int[] arr)
+{
+    int min = arr[0];
+    int max = arr[0];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
+    }
+
+    int result = max - min;
+
+    return result;
+}
+
+Console.WriteLine($"Разница между минимальным и максимальным элементом массива: { Diff(array) }");
